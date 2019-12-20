04bcfbc8093702a306a0a70f4e0f2ba36c5237fc
Fernando Santos <nanndoj@gmail.com>
2019-12-20 16:24:19 +0100

    Addin missing port to the proxy script

--- src/proxy-handler.ts
+++ src/proxy-handler.ts
@@ -1,56 +1,69 @@
-import {parseURL} from "./lib/parse-url";
-import {Request, Response} from "express";
-import {IURLObject} from "./types/IUrl";
-import {formatXRoadResponse, parseXRoadMessageBody} from "./lib/parse-message";
-import {IXroadRequest} from "./types/IXroadRequest";
+import { parseURL } from "./lib/parse-url";
+import { Request, Response } from "express";
+import { IURLObject } from "./types/IUrl";
+import {
+  formatXRoadResponse,
+  parseXRoadMessageBody
+} from "./lib/parse-message";
+import { IXroadRequest } from "./types/IXroadRequest";
 
-export const proxyMiddleware = (proxy: any) => (req: IXroadRequest, res: Response, next: Function) => {
-    const protocol = req.protocol;
-    const urlObject: IURLObject = parseURL(req.url);
+export const proxyMiddleware = (proxy: any) => (
+  req: IXroadRequest,
+  res: Response,
+  next: Function
+) => {
+  const protocol = req.protocol;
+  const urlObject: IURLObject = parseURL(req.url);
 
-    req.url = urlObject.path;
-    req.isWSDL = req.url.toUpperCase().endsWith('WSDL');
+  req.url = urlObject.path;
+  req.isWSDL = req.url.toUpperCase().endsWith("WSDL");
 
-    if(!req.isWSDL) {
-        let bufferArr: any[] = [];
-        req.on('data', function (data: any) {
-            bufferArr.push(data)
-        });
+  if (!req.isWSDL) {
+    let bufferArr: any[] = [];
+    req.on("data", function(data: any) {
+      bufferArr.push(data);
+    });
 
-        req.on('end', function () {
-            try {
-                const buffer = Buffer.concat(bufferArr);
-                const message = parseXRoadMessageBody(buffer.toString());
-                req.xroadRequestBody = message;
-            } catch (err) {
-                proxyErrorHandler(err, req, res);
-            }
-        });
-    }
-
-    try {
-        proxy.web(req, res, {
-            target: `${protocol}://${urlObject.host}`,
-            changeOrigin: true
-        }, (err: Error) => proxyErrorHandler(err, req, res));
-    } catch(err) {
+    req.on("end", function() {
+      try {
+        const buffer = Buffer.concat(bufferArr);
+        const message = parseXRoadMessageBody(buffer.toString());
+        req.xroadRequestBody = message;
+      } catch (err) {
         proxyErrorHandler(err, req, res);
-    }
+      }
+    });
+  }
+
+  const port = urlObject.port ? `:${urlObject.port}` : "";
+
+  try {
+    proxy.web(
+      req,
+      res,
+      {
+        target: `${protocol}://${urlObject.host}${port}`,
+        changeOrigin: true
+      },
+      (err: Error) => proxyErrorHandler(err, req, res)
+    );
+  } catch (err) {
+    proxyErrorHandler(err, req, res);
+  }
 };
 
 export const proxyResponseHandler = (data: any, req: IXroadRequest) => {
-    try {
-
-        if(req.isWSDL) {
-            return data;
-        }
-
-        return formatXRoadResponse(req.xroadRequestBody, data.toString());
-    } catch(err) {
-        return err;
+  try {
+    if (req.isWSDL) {
+      return data;
     }
+
+    return formatXRoadResponse(req.xroadRequestBody, data.toString());
+  } catch (err) {
+    return err;
+  }
 };
 
 export const proxyErrorHandler = (err: Error, req: Request, res: Response) => {
-    res.end(err.toString());
+  res.end(err.toString());
 };
