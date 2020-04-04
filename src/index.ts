d81af916eb7b9e1ef96e62b94533123440922c12
Fernando Santos <nanndoj@gmail.com>
2020-04-04 17:59:04 +0200

    Not rejecting unauthorized certificate

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
-import * as httpProxy from 'http-proxy';
-import express from 'express';
-import http from 'http';
-import https, { AgentOptions } from 'https';
-import path from 'path';
-import fs from 'fs';
-import {proxyErrorHandler, proxyMiddleware,  proxyResponseHandler} from "./proxy-handler";
+import * as httpProxy from "http-proxy";
+import express from "express";
+import http from "http";
+import https, { AgentOptions } from "https";
+import path from "path";
+import fs from "fs";
+import {
+  proxyErrorHandler,
+  proxyMiddleware,
+  proxyResponseHandler
+} from "./proxy-handler";
 //@ts-ignore
-import transformerProxy from 'transformer-proxy';
+import transformerProxy from "transformer-proxy";
 
 // Create a proxy server with custom application logic
 // @ts-ignore
 const proxy = httpProxy.createProxyServer({});
 const app = express();
 
-proxy.on('error', proxyErrorHandler);
+proxy.on("error", proxyErrorHandler);
 
 app.use(transformerProxy(proxyResponseHandler));
 // @ts-ignore
@@ -22,17 +26,25 @@ app.use(proxyMiddleware(proxy));
 // Create the http server
 const HTTP_PORT = process.env.HTTP_PORT || 5080;
 const httpServer = http.createServer(app);
-httpServer.listen(HTTP_PORT, () => console.log(`Proxy server listening on http port ${HTTP_PORT}`));
+httpServer.listen(HTTP_PORT, () =>
+  console.log(`Proxy server listening on http port ${HTTP_PORT}`)
+);
 
 // Create the https server
 const HTTPS_PORT = process.env.HTTPS_PORT || 5443;
-const KEY = process.env.KEY || path.join(__dirname, 'sslcert/local.key');
-const CERT = process.env.CERT || path.join(__dirname, 'sslcert/local.crt');
+const KEY = process.env.KEY || path.join(__dirname, "sslcert/local.key");
+const CERT = process.env.CERT || path.join(__dirname, "sslcert/local.crt");
 
-const privateKey  = fs.readFileSync(KEY, 'utf8');
-const certificate = fs.readFileSync(CERT, 'utf8');
+const privateKey = fs.readFileSync(KEY, "utf8");
+const certificate = fs.readFileSync(CERT, "utf8");
 
-const credentials: AgentOptions = {key: privateKey, cert: certificate};
+const credentials: AgentOptions = {
+  key: privateKey,
+  cert: certificate,
+  rejectUnauthorized: false
+};
 
 const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(HTTPS_PORT, () => console.log(`Proxy server listening on https port ${HTTPS_PORT}`));
+httpsServer.listen(HTTPS_PORT, () =>
+  console.log(`Proxy server listening on https port ${HTTPS_PORT}`)
+);
